d76bd96a4b95139d2525ad730f23fa6e76a881e9
Vasyl <vasjanja@gmail.com>
2021-05-18 21:05:02 +0300

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,129 @@
 <template>
-  <AppTable>
-    <template #dataCheckbox>
+  <AppTable :columns="columns" :dataArray="dataArray">
+    <!-- column slots -->
+
+    <template #select_column="{column}">
+      {{ column.label }}
+      <input type="checkbox" />
+    </template>
+
+    <template #preview_column="{column}">
+      {{ column.label }}
+    </template>
+
+    <template #tags_column="{column}">
+      {{ column.label }}
+    </template>
+
+    <template #triggers_column="{column}">
+      {{ column.label }}
+    </template>
+
+    <template #personas_column="{column}">
+      {{ column.label }}
+    </template>
+
+    <template #logicSets_column="{column}">
+      {{ column.label }}
+    </template>
+
+    <template #createdAt_column="{column}">
+      {{ column.label }}
+    </template>
+
+    <template #updatedAt_column="{column}">
+      {{ column.label }}
+    </template>
+
+    <!-- below are row slots -->
+
+    <template #select_row>
       <div class="inputField">
         <input type="checkbox" />
       </div>
       <div>...</div>
     </template>
 
-    <template #dataPreview="{preview}">
-      <div v-if="preview">{{ preview }}</div>
+    <template #preview_row="{row}">
+      <div v-if="row.preview">{{ row.preview }}</div>
     </template>
 
-    <template #dataTags="tags">
-      <div v-if="tags">
-        <div v-for="tag in tags" :key="tag" class="tags">
-          <div v-for="everyTag in tag" :key="everyTag">
-            {{ everyTag }}
+    <template #tags_row="{row}">
+      <div v-if="row.tags">
+        <div v-for="tag in row.tags" :key="tag" class="tags">
+          <div>
+            {{ tag }}
           </div>
         </div>
       </div>
     </template>
 
-    <template #dataTriggers="{triggers}">
-      <div v-if="triggers">
-        <div v-if="triggers.length > 1 && !triggers.show">
-          <div>{{ triggers[0] }}</div>
-          <div @click.prevent="showAll(triggers)">
-            <a href=""> + {{ triggers.length - 1 }} more </a>
+    <template #triggers_row="{row}">
+      <div v-if="row.triggers">
+        <div v-if="row.triggers.length > 1 && !row.triggers.show">
+          <div>{{ row.triggers[0] }}</div>
+          <div @click.prevent="showAll(row.triggers)">
+            <a href=""> + {{ row.triggers.length - 1 }} more </a>
           </div>
         </div>
+
         <div v-else>
-          <div v-for="trigger in triggers" :key="trigger">
+          <div v-for="trigger in row.triggers" :key="trigger">
             {{ trigger }}
           </div>
         </div>
       </div>
     </template>
 
-    <template #dataPersonas="personas">
-      <div v-if="personas.personas">
-        <div v-if="personas.personas.length > 1 && !personas.personas.show">
-          <div>{{ personas.personas[0] }}</div>
-          <div @click.prevent="showAll(personas.personas)">
-            <a href=""> + {{ personas.personas.length - 1 }} more </a>
+    <template #personas_row="{row}">
+      <div v-if="row.personas">
+        <div v-if="row.personas.length > 1 && !row.personas.show">
+          <div>{{ row.personas[0] }}</div>
+          <div @click.prevent="showAll(row.personas)">
+            <a href=""> + {{ row.personas.length - 1 }} more </a>
           </div>
         </div>
-        <div v-else v-for="each in personas.personas" :key="each">
+        <div v-else v-for="each in row.personas" :key="each">
           {{ each }}
         </div>
       </div>
     </template>
 
-    <template #dataLogic="{logicSets}">
-      <div v-if="logicSets">
-        <div v-if="logicSets.length > 1 && !logicSets.show">
-          <div>{{ logicSets[0] }}</div>
-          <div @click.prevent="showAll(logicSets)">
-            <a href=""> + {{ logicSets.length - 1 }} more </a>
+    <template #logicSets_row="{row}">
+      <div v-if="row.logicSets">
+        <div v-if="row.logicSets.length > 1 && !row.logicSets.show">
+          <div>{{ row.logicSets[0] }}</div>
+          <div @click.prevent="showAll(row.logicSets)">
+            <a href=""> + {{ row.logicSets.length - 1 }} more </a>
           </div>
         </div>
 
-        <div v-else v-for="each in logicSets" :key="each">
+        <div v-else v-for="each in row.logicSets" :key="each">
           {{ each }}
         </div>
       </div>
     </template>
 
-    <template #dataCreated="createdAt">
-      <div v-for="each in createdAt" :key="each">
-        {{ createdAt.createdAt.toLocaleDateString() }}
-      </div>
+    <template #createdAt_row="{row}">
+      {{
+        row.createdAt.toLocaleDateString('en-US', {
+          month: 'short',
+          day: '2-digit',
+          year: 'numeric',
+        })
+      }}
     </template>
 
-    <template #dataUpdated="updatedAt">
-      <div>{{ updatedAt.updatedAt.toLocaleDateString() }}</div>
+    <template #updatedAt_row="{row}">
+      <div>
+        {{
+          row.updatedAt.toLocaleDateString('en-US', {
+            month: 'short',
+            day: '2-digit',
+            year: 'numeric',
+          })
+        }}
+      </div>
     </template>
   </AppTable>
 </template>
@@ -88,6 +138,77 @@ export default {
     showAll(data) {
       data.show = true
     },
+  },
+  data() {
+    return {
+      dataArray: [
+        {
+          preview:
+            'Some dummy sasdas dsdfjdf sjkdf ksjdhf kjlszdhfkjl sdhfjkjdsjfj hdfkjghskhf usedhf kusdjhfj sdhfj   sdfhs dftring',
+          tags: ['tag1', 'tag2', 'jk'],
+          triggers: ['trigger1'],
+          personas: ['persona1', 'persona8', 'persona16'],
+          logicSets: ['logicSet1', 'logiclohi'],
+          createdAt: new Date(2010, 9, 8),
+          updatedAt: new Date(2015, 9, 7),
+        },
+        {
+          preview: 'Some dummy string',
+          tags: ['tag1', 'tag2'],
+          triggers: ['trigger2', 'trig5'],
+          personas: ['persona2'],
+          logicSets: ['logicSet1'],
+          createdAt: new Date(2017, 5, 5),
+          updatedAt: new Date(2018, 5, 7),
+        },
+        {
+          preview: 'Some dummy string',
+          tags: ['tag1', 'tag2'],
+          triggers: ['trigger3'],
+          personas: ['persona3'],
+          logicSets: ['logicSet1'],
+          createdAt: new Date(2020, 4, 4),
+          updatedAt: new Date(2021, 5, 5),
+        },
+        {
+          preview: 'Some dummy string',
+          tags: ['tag1', 'tag2'],
+          triggers: ['trigger4'],
+          personas: ['persona4'],
+          logicSets: ['logicSet1'],
+          createdAt: new Date(2020, 6, 12),
+          updatedAt: new Date(2020, 11, 28),
+        },
+        {
+          preview: 'Some dummy string',
+          tags: ['tag1', 'tag2'],
+          triggers: ['trigger5'],
+          personas: ['persona5', 'persona99', 'persona77', 'personaaaaaa'],
+          logicSets: ['logicSet1'],
+          createdAt: new Date(2021, 1, 1),
+          updatedAt: new Date(2021, 3, 8),
+        },
+        {
+          preview: 'Some dummy string',
+          tags: ['tag1', 'tag2'],
+          triggers: ['trigger6'],
+          personas: ['persona6'],
+          logicSets: ['logicSet1'],
+          createdAt: new Date(2021, 5, 1),
+          updatedAt: new Date(2021, 5, 9),
+        },
+      ],
+      columns: [
+        { prop: 'select', label: '', width: 55 },
+        { prop: 'preview', label: 'Preview', minWidth: 240 },
+        { prop: 'tags', label: 'Tags', minWidth: 170 },
+        { prop: 'triggers', label: 'Triggers', minWidth: 170 },
+        { prop: 'personas', label: 'Personas', minWidth: 170 },
+        { prop: 'logicSets', label: 'Logic sets', minWidth: 170 },
+        { prop: 'createdAt', label: 'Created date', minWidth: 150 },
+        { prop: 'updatedAt', label: 'Last modified', minWidth: 150 },
+      ],
+    }
   },
 }
 </script>
@@ -109,6 +230,7 @@ export default {
 }
 
 .inputField {
+  display: block;
   width: 55px;
 }
 
--- src/components/AppTable.vue
+++ src/components/AppTable.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="table-wrapper">
-    <h1>Here will be the table</h1>
+    <h1>Here will be the slot-scoped table</h1>
     <table>
       <thead>
         <tr>
-          <th v-for="column in columns" :key="column.prop" :class="column.prop">
-            {{ column.label }}
+          <th
+            v-for="(column, index) in columns"
+            :key="index"
+            :style="{
+              'min-width': column.minWidth + 'px',
+              width: column.width + 'px',
+            }"
+          >
+            <slot :name="column.prop + '_column'" :column="column" x />
           </th>
         </tr>
       </thead>
 
-      <tr v-for="each in dataArray" :key="each.personas">
-        <td><slot name="dataCheckbox" /></td>
-        <td><slot name="dataPreview" :preview="each.preview" /></td>
-        <td><slot name="dataTags" :tags="each.tags" /></td>
-        <td><slot name="dataTriggers" :triggers="each.triggers" /></td>
-        <td><slot name="dataPersonas" :personas="each.personas" /></td>
-        <td><slot name="dataLogic" :logicSets="each.logicSets" /></td>
-        <td><slot name="dataCreated" :createdAt="each.createdAt" /></td>
-        <td><slot name="dataUpdated" :updatedAt="each.updatedAt" /></td>
+      <tr v-for="(eachRow, index) in dataArray" :key="index">
+        <td v-for="column in columns" :key="column">
+          <slot :name="column.prop + '_row'" :row="eachRow" />
+        </td>
       </tr>
     </table>
   </div>
@@ -26,77 +28,7 @@
 
 <script>
 export default {
-  data() {
-    return {
-      dataArray: [
-        {
-          preview:
-            'Some dummy sasdas dsdfjdf sjkdf ksjdhf kjlszdhfkjl sdhfjkjdsjfj hdfkjghskhf usedhf kusdjhfj sdhfj   sdfhs dftring',
-          tags: ['tag1', 'tag2', 'jk'],
-          triggers: ['trigger1'],
-          personas: ['persona1', 'persona8', 'persona16'],
-          logicSets: ['logicSet1', 'logiclohi'],
-          createdAt: new Date(2010, 9, 8),
-          updatedAt: new Date(2015, 9, 7),
-        },
-        {
-          preview: 'Some dummy string',
-          tags: ['tag1', 'tag2'],
-          triggers: ['trigger2', 'trig5'],
-          personas: ['persona2'],
-          logicSets: ['logicSet1'],
-          createdAt: new Date(2017, 5, 5),
-          updatedAt: new Date(2018, 5, 7),
-        },
-        {
-          preview: 'Some dummy string',
-          tags: ['tag1', 'tag2'],
-          triggers: ['trigger3'],
-          personas: ['persona3'],
-          logicSets: ['logicSet1'],
-          createdAt: new Date(2020, 4, 4),
-          updatedAt: new Date(2021, 5, 5),
-        },
-        {
-          preview: 'Some dummy string',
-          tags: ['tag1', 'tag2'],
-          triggers: ['trigger4'],
-          personas: ['persona4'],
-          logicSets: ['logicSet1'],
-          createdAt: new Date(2020, 6, 12),
-          updatedAt: new Date(2020, 11, 28),
-        },
-        {
-          preview: 'Some dummy string',
-          tags: ['tag1', 'tag2'],
-          triggers: ['trigger5'],
-          personas: ['persona5', 'persona99', 'persona77', 'personaaaaaa'],
-          logicSets: ['logicSet1'],
-          createdAt: new Date(2021, 1, 1),
-          updatedAt: new Date(2021, 3, 8),
-        },
-        {
-          preview: 'Some dummy string',
-          tags: ['tag1', 'tag2'],
-          triggers: ['trigger6'],
-          personas: ['persona6'],
-          logicSets: ['logicSet1'],
-          createdAt: new Date(2021, 5, 1),
-          updatedAt: new Date(2021, 5, 9),
-        },
-      ],
-      columns: [
-        { prop: 'select', label: '', width: 55 },
-        { prop: 'preview', label: 'Preview', minWidth: 240 },
-        { prop: 'tags', label: 'Tags', minWidth: 170 },
-        { prop: 'triggers', label: 'Triggers', minWidth: 170 },
-        { prop: 'personas', label: 'Personas', minWidth: 170 },
-        { prop: 'logicSets', label: 'Logic sets', minWidth: 170 },
-        { prop: 'createdAt', label: 'Created date', minWidth: 150 },
-        { prop: 'updatedAt', label: 'Last modified', minWidth: 150 },
-      ],
-    }
-  },
+  props: ['dataArray', 'columns'],
 }
 </script>
 
@@ -116,8 +48,6 @@ th {
 td {
   border: solid 1px gray;
   border-style: solid none;
-  border-radius: ;
-  padding: 10px;
   background-color: whitesmoke;
 }
 td:first-child {
@@ -130,21 +60,5 @@ td:last-child {
   border-right-style: solid;
   border-bottom-right-radius: 10px;
   border-top-right-radius: 10px;
-}
-
-.preview {
-  min-width: 240px;
-}
-
-.tags,
-.triggers,
-.personas,
-.logicSets {
-  min-width: 170px;
-}
-
-.createdAt,
-.updatedAt {
-  min-width: 150px;
 }
 </style>
